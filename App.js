4003cde152b84e282771076cc219986c0abf1cd6
Kamil Parczewski <kamil.parczewski@o2.pl>
2019-04-24 21:00:14 +0200

    02-components - input

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+import { colors } from './helpers'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      BMI: '',
+      weights: [],
+    }
+  }
+
+  onChangeBMI = (inputValue) => {
+    this.setState({
+      BMI: inputValue,
+    })
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello React Native 😎</Text>
+      <View style={styles.mainView}>
+        <View style={styles.container}>
+          <Text style={styles.label}>BMI</Text>
+          <TextInput
+            onChangeText={this.onChangeBMI}
+            value={this.state.BMI}
+            style={styles.input}
+            keyboardType="numeric"
+            placeholder="Please type your BMI"
+            placeholderTextColor={colors.blue}
+          />
+
+          <Text>BMI: {this.state.BMI}</Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    backgroundColor: colors.lightblue,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    marginVertical: 50,
+    marginHorizontal: 20
+  },
+  input: {
+    height: 40,
+    width: `100%`,
+    fontSize: 15,
+    borderBottomWidth: 3,
+    borderRadius: 3,
+    borderColor: colors.blue,
+  },
+  label: {
+    width: '100%',
+    justifyContent: 'flex-start',
+    fontWeight: 'bold',
+    color: colors.blue
   },
 });
---
+++ helpers/index.js
@@ -0,0 +1,5 @@
+// Colors
+export const colors = {
+  blue: '#0080FF',
+  lightblue: '#d7e8ff'
+};
